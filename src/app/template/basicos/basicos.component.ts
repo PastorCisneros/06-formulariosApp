6e21f24e653b8bbd51e63cb64bb5caefecea3ec6
Pastor Cisneros <pasada.65@gmail.com>
2022-11-01 17:30:06 -0600

    finish form dinamicos

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario!:NgForm;
+
+  initForm={
+    producto:'RXT 4080ti',
+    precio:0,
+    existencias:10
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  nombreValido():boolean{
+    return this.miFormulario?.form.controls['producto']?.invalid &&
+           this.miFormulario?.form.controls['producto']?.touched;
+  }
+
+
+  precioValido():boolean{
+    return this.miFormulario?.form.controls['precio']?.invalid &&
+           this.miFormulario?.form.controls['precio']?.touched;
+
+  }
+
+  // guardar(miFormulario:NgForm){
+    guardar(){
+      console.log(this.miFormulario);
+      this.miFormulario.resetForm({
+        producto:'',
+        precio:0,
+        existencias:0
+      });
+    }
+
 
 }
