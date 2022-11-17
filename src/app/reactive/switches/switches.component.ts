ab8c0046745700eb74a3aecd4decc953c649db67
Pastor Cisneros <pasada.65@gmail.com>
2022-11-17 15:10:30 -0600

    finish reactive forms

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent implements OnInit  {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  miFormulario:FormGroup = this.fb.group({
+    genero:['M', Validators.required],
+    notificaciones:[false,Validators.required],
+    condiciones:[false,Validators.requiredTrue],
+
+  });
+
+  persona ={
+    genero:'F',
+    notificaciones:true,
+    }
+
+  constructor(private fb:FormBuilder) { }
+
+  ngOnInit() {
+    this.miFormulario.reset({
+      ...this.persona,
+      condiciones:false,
+    });
+
+
+    this.miFormulario.valueChanges.subscribe(form=>{
+      delete form.condiciones;
+      this.persona=form;
+      console.log(form);
+    })
+  }
+
+
+  guardar(){
+    const miForm = {...this.miFormulario.value};
+
+    delete miForm.condiciones;
+
+    this.persona=miForm;
+    console.log(miForm);
   }
 
 }
